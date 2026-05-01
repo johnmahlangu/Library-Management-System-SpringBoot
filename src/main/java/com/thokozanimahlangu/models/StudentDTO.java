5628570db6c0eed9bc50ef4ccda8447e286336f6
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-05-01 15:18:46 +0200

    added studentDTO

---
+++ src/main/java/com/thokozanimahlangu/models/StudentDTO.java
@@ -0,0 +1,31 @@
+package com.thokozanimahlangu.models;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data Transfer Object for Student entity.
+ * Used for transporting student between the REST layer and the Service layer.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class StudentDTO {
+
+	private UUID studentId;
+	
+	@NotBlank
+	private String firstName;
+	
+	@NotBlank
+	private String lastName;
+	
+	@NotBlank
+	private String studentEmail;
+}
