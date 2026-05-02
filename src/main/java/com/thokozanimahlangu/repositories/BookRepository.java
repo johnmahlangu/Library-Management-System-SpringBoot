c35e8411ade17688bff1319d9830935ed5a984dd
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-05-02 14:24:24 +0200

    add search functionality

--- src/main/java/com/thokozanimahlangu/repositories/BookRepository.java
+++ src/main/java/com/thokozanimahlangu/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package com.thokozanimahlangu.repositories;
 
+import java.time.Year;
 import java.util.List;
 import java.util.UUID;
 
@@ -9,17 +10,39 @@ import com.thokozanimahlangu.entities.Book;
 
 /**
  * Repository interface for Book entity.
+ * Provides automated query execution for searching books based on various attributes.
  * Provides abstraction for database operations using Spring Data JPA.
  */
 
 public interface BookRepository extends JpaRepository<Book, UUID>{
 
-	//Searches for books by title(case-insensitive) and returns matching books
+	//Searches for books by title and returns matching books
 	List<Book> findAllByTitleIsLikeIgnoreCase(String title);
 	
-	//Searches for books by author(case-insensitive) and returns matching books
+	//Searches for books by year published and returns matching books
+	List<Book> findAllByYear(Year publicationYear);
+	
+	//Searches for books by ISBN and returns matching books
+	List<Book> findAllByISBN(String ISBN);
+	
+	//Searches for books by author and returns matching books
 	List<Book> findAllByAuthorIsLikeIgnoreCase(String author);
 	
-	//Searches for books by title and author (case-insensitive) and returns matching books
+	//Searches for books by title and publication year and returns matching books
+	List<Book> findAllByTitleIsLikeIgnoreCaseAndYear(String title, Year publicationYear);
+	
+	//Searches for books by title and ISBN and returns matching books
+	List<Book> findAllByTitleIsLikeIgnoreCaseAndISBN(String title, String ISBN);
+	
+	//Searches for books by year and ISBN and returns matching books
+	List<Book> findAllByYearAndISBN(Year publicationYear, String ISBN);
+	
+	//Searches for books by year and author and returns matching books
+	List<Book> findAllByYearAndAuthorIsLikeIgnoreCase(Year publicationYear, String author);
+	
+	//Searches for books by ISBN and and author and returns matching books
+	List<Book> findAllByISBNAndAuthorIsLikeIgnoreCase(String ISBN, String author);
+	
+	//Searches for books by title and author and returns matching books
 	List<Book> findAllByTitleIsLikeIgnoreCaseAndAuthorIsLikeIgnoreCase(String title, String author);
 }
