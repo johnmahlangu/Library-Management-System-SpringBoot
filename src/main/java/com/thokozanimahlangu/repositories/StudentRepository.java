9796765470cb0b0c9d3b127031b8ea77ee4dea78
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-05-04 11:22:29 +0200

    refactor to use JpaSpecification for searching students

--- src/main/java/com/thokozanimahlangu/repositories/StudentRepository.java
+++ src/main/java/com/thokozanimahlangu/repositories/StudentRepository.java
@@ -1,39 +1,18 @@
 package com.thokozanimahlangu.repositories;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.thokozanimahlangu.entities.Student;
 
 /**
  * Repository interface for Student entity.
- * Provides abstraction for database operations using Spring Data JPA.
+ * Provides automated query execution for searching students based on various attributes.
  */
 
-public interface StudentRepository extends JpaRepository<Student, UUID>{
+public interface StudentRepository extends JpaRepository<Student, UUID>, JpaSpecificationExecutor<Student>{
 
-	//Searches for students by firstName(case-insensitive) and returns matching students
-	List<Student> findAllByFirstNameIsLikeIgnoreCase(String firstName);
-	
-	//Searches for students by lastName(case-insensitive) and returns matching students
-	List<Student> findAllByLastNameIsLikeIgnoreCase(String lastName);
-	
-	//Searches for students by email(case-insensitive) and returns matching students
-	List<Student> findAllByEmailIsLikeIgnoreCase(String email);
-	
-	//Searches for students by firstName and last name (all case-insensitive) and returns matching students
-	List<Student> findAllByFirstNameIsLikeIgnoreCaseAndLastNameIsLikeIgnoreCase(String firstName, String lastName);
-	
-	//Searches for students by firstName and email (all case-insensitive) and returns matching students
-	List<Student> findAllByFirstNameIsLikeIgnoreCaseAndEmailIsLikeIgnoreCase(String firstName, String email);
-	
-	//Searches for students by last name and email (all case-insensitive) and returns matching students
-	List<Student> findAllByLastNameIsLikeIgnoreCaseAndEmailIsLikeIgnoreCase(String firstName, String lastName);
-	
-	//Searches for students by first name, last name and email (all case-insensitive) and returns matching students
-	List<Student> findAllByFirstNameIsLikeIgnoreCaseAndLastNameIsLikeIgnoreCaseAndEmailIsLikeIgnoreCase(String firstName, String lastName, String email);
-	
-	
+		
 }
