e1ecbe6584661df04ebc2edd6af0dacc4aee86bc
Thokozani Mahlangu <122809723+johnmahlangu@users.noreply.github.com>
2026-06-23 18:57:49 +0200

    Update SetUpSQL.sql
    make email field unique, add status field to issuebook table

--- SetUpSQL.sql
+++ SetUpSQL.sql
@@ -7,6 +7,7 @@ CREATE TABLE Book (
     isbn				varchar(13)				default null,
     created_date		datetime(6)				default null,
     update_date			datetime(6)				default null,
+
     publication_year	smallint				default null
 )engine = InnoDB;
 
@@ -14,9 +15,10 @@ CREATE TABLE Student (
     id 					varchar(36) 			primary key not null,
     first_name	 		varchar(100)			default null,
     last_name			varchar(100)			default null,
-    email 				varchar(100)			default null,
+    email 				varchar(100)			default null unique,
     created_date		datetime(6)				default null,
     update_date			datetime(6)				default null
+    constraint uc_student_email unique (email)
 )engine = InnoDB;
 
 CREATE TABLE IssueBook (
@@ -24,9 +26,10 @@ CREATE TABLE IssueBook (
     book_id 			varchar(36)				not null,
     student_id 			varchar(36)				not null,
     issue_date 			datetime(6)				default null,
+    status              varchar(255),
     update_date		 	datetime(6)				default null,
     due_date			date					default null,
     return_date			date					default null,
     constraint fk_book foreign key (book_id)  references Book(id),
     constraint fk_student foreign key (student_id) references Student(id)
-);
+)engine = InnoDB;
