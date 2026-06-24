16e0dcb4deaadf09f9b2fabdb667a29590f587ec
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-06-24 19:36:11 +0200

    fix: import NotFoundException

--- src/main/java/com/thokozanimahlangu/controllers/StudentController.java
+++ src/main/java/com/thokozanimahlangu/controllers/StudentController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thokozanimahlangu.exceptions.NotFoundException;
 import com.thokozanimahlangu.models.StudentDTO;
 import com.thokozanimahlangu.services.StudentService;
 
