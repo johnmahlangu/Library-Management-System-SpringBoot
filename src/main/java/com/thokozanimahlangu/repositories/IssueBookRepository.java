8c9fa4096885851d59df09df9394dc24e47f7fa9
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-06-24 19:27:17 +0200

    check whether a book is issued

--- src/main/java/com/thokozanimahlangu/repositories/IssueBookRepository.java
+++ src/main/java/com/thokozanimahlangu/repositories/IssueBookRepository.java
@@ -1,6 +1,7 @@
 package com.thokozanimahlangu.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,4 +22,6 @@ public interface IssueBookRepository extends JpaRepository<IssueBook,UUID>{
 	List<IssueBook> findByStudentId(UUID studentId);
 	
 	List<IssueBook> findByStudentIdAndReturnDateIsNull(UUID studentId);
+	
+	Optional<IssueBook> findByBookIdAndReturnDateIsNull(UUID bookId);
 }
