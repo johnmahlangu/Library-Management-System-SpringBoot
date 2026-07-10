2c943d7d3766bbbe2274c043ac2c2c54165790bf
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-07-10 19:06:20 +0200

    feat: use bookId to get book details on the html template

--- src/main/java/com/thokozanimahlangu/web/BookViewController.java
+++ src/main/java/com/thokozanimahlangu/web/BookViewController.java
@@ -1,10 +1,15 @@
 package com.thokozanimahlangu.web;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.thokozanimahlangu.exceptions.NotFoundException;
+import com.thokozanimahlangu.models.BookDTO;
 import com.thokozanimahlangu.services.BookService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +19,9 @@ import lombok.RequiredArgsConstructor;
 public class BookViewController {
 
 	public static final String BOOKS_PATH = "/books";
+	public static final String BOOK_PATH_ID = BOOKS_PATH + "/{bookId}";
 	public static final String BOOKS_LIST_VIEW = "books/list"; 
+	public static final String BOOK_DETAILS_VIEW = "books/details";
 	
 	private final BookService bookService;
 	
@@ -37,9 +44,28 @@ public class BookViewController {
 							Model model) {
 		
 		// Fetch filtered books from the service layer and add them to the UI model
-		model.addAttribute("books", bookService.listBooks(title, author, isbn, publicationYear));
-		
+		model.addAttribute("books", bookService.listBooks(title, author, isbn, publicationYear));		
 		// Render the book list HTML template
 		return BOOKS_LIST_VIEW;
+	}
+	
+	/**
+	 * Handles GET requests to retrieve and display details for a specific book.
+	 *
+	 * @param bookId the unique identifier (UUID) of the book to retrieve
+	 * @param model  the Spring UI Model to pass data to the view
+	 * @return the path to the book details view template
+	 */
+	@GetMapping(BOOK_PATH_ID)
+	public String getBook(@PathVariable UUID bookId, Model model) {
+		
+		// Fetch the book by ID; if it does not exist, throw a NotFoundException
+		BookDTO book = bookService.getBookById(bookId)
+				   .orElseThrow(NotFoundException::new);
+		
+		// Add existing book to the UI model
+		model.addAttribute("book", book);
+		// Render the book details HTML template
+		return BOOK_DETAILS_VIEW;
 	}
 }
