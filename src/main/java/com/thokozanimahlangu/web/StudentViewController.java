6c0ed5254729d06515d3eb0860e12003cf3c3497
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-07-12 20:06:45 +0200

    feat: list students by optional filtering using html template

---
+++ src/main/java/com/thokozanimahlangu/web/StudentViewController.java
@@ -0,0 +1,42 @@
+package com.thokozanimahlangu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.thokozanimahlangu.services.StudentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class StudentViewController {
+
+	public static final String STUDENTS_PATH = "/students";
+	public static final String STUDENT_LIST_VIEW = "students/list";
+	
+	private final StudentService studentService;
+	
+	/**
+	 * Handles GET requests to list students. Supports optional filtering by first name, last name and email
+	 *
+	 * @param firstName           	optional filter for student's first name
+	 * @param lastName         		optional filter for student's last name
+	 * @param email			 		optional filter for student's email
+	 * 
+	 * @return the path to the student list view template
+	 */
+	@GetMapping(STUDENTS_PATH)
+	public String listStudents(@RequestParam(required = false) String firstName,
+							   @RequestParam(required = false) String lastName,
+							   @RequestParam(required = false) String email,
+							   Model model) {
+		// Fetch filtered students from the service layer and add them to the UI model
+		model.addAttribute("listStudents", studentService.listStudents(firstName, lastName, email));
+		// Render the student list HTML template
+		return STUDENT_LIST_VIEW;
+	}
+	
+								
+}
