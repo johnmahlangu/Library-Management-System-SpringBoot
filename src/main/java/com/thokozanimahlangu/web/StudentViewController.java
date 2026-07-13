d01ad3c667467c702ee2e9c7e326bf71889e00e7
Thokozani Mahlangu <thokozanimahlangu0608@gmail.com>
2026-07-13 14:00:11 +0200

    feat: get a student using student's ID

--- src/main/java/com/thokozanimahlangu/web/StudentViewController.java
+++ src/main/java/com/thokozanimahlangu/web/StudentViewController.java
@@ -1,10 +1,15 @@
 package com.thokozanimahlangu.web;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.thokozanimahlangu.exceptions.NotFoundException;
+import com.thokozanimahlangu.models.StudentDTO;
 import com.thokozanimahlangu.services.StudentService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +19,10 @@ import lombok.RequiredArgsConstructor;
 public class StudentViewController {
 
 	public static final String STUDENTS_PATH = "/students";
+	public static final String STUDENT_PATH_ID = STUDENTS_PATH + "/{studentId}";
 	public static final String STUDENT_LIST_VIEW = "students/list";
+	public static final String STUDENT_DETAILS_VIEW = "/students/details";
+	
 	
 	private final StudentService studentService;
 	
@@ -38,5 +46,22 @@ public class StudentViewController {
 		return STUDENT_LIST_VIEW;
 	}
 	
-								
+	/**
+	 * Handles GET requests to retrieve and display details for a specific student.
+	 *
+	 * @param studentId 			the unique identifier (UUID) of the student to retrieve
+	 * @param model  				the Spring UI Model to pass data to the view
+	 * @throws NotFoundException 	if no student matches the provided UUID
+	 * @return the path to the student details view template
+	 */
+	@GetMapping(STUDENT_PATH_ID)		
+	public String getStudent(@PathVariable UUID studentId, Model model) {
+		
+		// Fetch student by ID; if the student does not exist, throw a NotFoundException
+		StudentDTO student = studentService.getStudentByID(studentId).orElseThrow(NotFoundException::new);
+		// Add existing book to the UI model;
+		model.addAttribute("student", student);
+		// Render the student details HTML template
+		return STUDENT_DETAILS_VIEW;
+	}
 }
